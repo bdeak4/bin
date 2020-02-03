7c88ec238e12d270f0832380658533dd5ffe35f7
bartol <b@bartol.dev>
2020-02-03 23:40:13 +0100

    Add ~/.local/bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+# prompt
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_prompt
@@ -7,10 +8,16 @@ function fish_prompt
     (set_color normal) (test $USER = 'root' && echo -n '#' || echo -n '$') ' '
 end
 
+# abbreviations
 abbr -a g git
+abbr -a n nnn
 abbr -a v nvim
 abbr -a t tmux
 abbr -a y yarn
+abbr -a m neomutt
+
+# environment
+set -x PATH $PATH ~/.local/bin
 
 set -x EDITOR nvim
 set -x PAGER less
@@ -21,6 +28,7 @@ else
   set -x BROWSER 'Firefox Developer Edition'
 end
 
+# fzf
 if command -v fzf >/dev/null
     set -x FZF_DEFAULT_COMMAND "rg --files --follow --hidden -g '!.git'"
     set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
@@ -31,17 +39,16 @@ if command -v fzf >/dev/null
     --bind 'ctrl-a:select-all'"
 end
 
+# nnn
 if command -v nnn >/dev/null
-    set -x NNN_USE_EDITOR 1
+    set -x NNN_COLORS '4231'
+    set -x NNN_BMS "d:~/dev/;D:~/downloads/;c:~/config/;s:~/config/scripts"
     set -x NNN_TRASH 1
-    set -x NNN_CONTEXT_COLORS '4231'
-    set -x NNN_BMS \
-    "d:$HOME/dev/;D:$HOME/downloads/;c:$HOME/config/;s:$HOME/config/scripts"
+    set -x NNN_USE_EDITOR 1
 end
 
 # colored man pages
-set -x LESS_TERMCAP_mb \e'[01;31m'
-set -x LESS_TERMCAP_md \e'[01;38;5;74m'
+set -x LESS_TERMCAP_md \e'[01;34m'
 set -x LESS_TERMCAP_me \e'[0m'
-set -x LESS_TERMCAP_so \e'[38;5;148m'
+set -x LESS_TERMCAP_so \e'[01;33m'
 set -x LESS_TERMCAP_se \e'[0m'
