f7f2d8d8666f3021185997644e3168e515ecf6b1
bartol <b@bartol.dev>
2020-05-01 22:05:17 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+set -x PATH ~/.local/bin $PATH
+set -x EDITOR kak
+
+abbr -a g git
+abbr -a gs git status
+abbr -a ga git add
+abbr -a gc git commit
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gl git log --graph --oneline --decorate --all
+abbr -a gp git push
+abbr -a gpom git push origin master
+abbr -a gpl git pull
+
+# colored man pages
+set -x LESS_TERMCAP_md \e'[01;36m'
+set -x LESS_TERMCAP_me \e'[0m'
+set -x LESS_TERMCAP_so \e'[01;32m'
+set -x LESS_TERMCAP_se \e'[0m'
+
+# ripgrep
+set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
+
+# go
+set -x GOBIN ~/.local/bin
+
+# rust
+set -x PATH ~/.cargo/bin $PATH
+
+# yarn
+set -x PATH ~/.yarn/bin $PATH
