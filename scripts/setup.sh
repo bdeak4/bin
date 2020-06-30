977e61eacf5bcda4a4ba0883073661579882e3a4
bartol <b@bartol.dev>
2020-06-30 21:01:42 +0200

    edit setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,23 +3,18 @@
 # dotfiles
 DOTFILES=$HOME/dotfiles
 git clone https://github.com/bartol/dotfiles $DOTFILES
-ln -svi $DOTFILES/.vimrc $HOME
-ln -svi $DOTFILES/.zshrc $HOME
-ln -svi $DOTFILES/.gitconfig $HOME
-ln -svi $DOTFILES/.editorconfig $HOME
-ln -svi $DOTFILES/.tmux.conf $HOME
-ln -svi $DOTFILES/.tmate.conf $HOME
+ln -s $DOTFILES/.vimrc $HOME
+ln -s $DOTFILES/.zshrc $HOME
+ln -s $DOTFILES/.gitconfig $HOME
+ln -s $DOTFILES/.editorconfig $HOME
+ln -s $DOTFILES/.tmux.conf $HOME
+ln -s $DOTFILES/.tmate.conf $HOME
 
 # vim
-mkdir -pv $HOME/.vim/undo
+mkdir -p $HOME/.vim/undo
 git clone https://github.com/fatih/vim-go $HOME/.vim/pack/plugins/start/vim-go
 git clone https://github.com/editorconfig/editorconfig-vim $HOME/.vim/pack/plugins/start/editorconfig-vim
 
 # projects
 PROJECTS=$HOME/dev
-mkdir -pv $PROJECTS
-cd $PROJECTS
-git clone https://github.com/bartol/bartol.dev
-git clone https://github.com/bartol/amadeus2.hr
-git clone https://github.com/bartol/sandbox
-cd -
+mkdir $PROJECTS
