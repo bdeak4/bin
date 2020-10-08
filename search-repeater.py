64e51b850b765296d38422073deb32e195c9e87b
Bartol Deak <b@bdeak.net>
2020-10-08 23:40:41 +0200

    handle submit

--- search-repeater.py
+++ search-repeater.py
@@ -9,18 +9,32 @@ from tkinter import ttk, font
 config = ConfigParser()
 config.read('config.ini')
 
-sites_available = list(dict(config.items('sites')).keys())
+sites_available = list(config.items('sites'))
 sites_selected = []
 
 ### handle submit
 
 def submit(event=None):
     q = query.get()
+    urls = []
 
     for i, site in enumerate(sites_selected):
         if site.get() != '0':
-            print(sites_available[i])
+            url, encode_type = sites_available[i][1].split('\n')
 
+            if encode_type == 'quote_plus':
+                encoded_query = urllib.parse.quote_plus(q)
+
+            urls.append(url.replace('QUERY', encoded_query))
+
+
+    # open new window and first tab
+    webbrowser.open_new(urls[0])
+
+    # wait for window and open other tabs
+    time.sleep(0.5)
+    for url in urls[1:]:
+        webbrowser.open_new_tab(url)
 
 ### ui
 
@@ -41,7 +55,7 @@ ttk.Button(root, text='search', command=submit, style="TButton").grid(column=2, 
 for i, site in enumerate(sites_available):
     sites_selected.append(tk.StringVar())
     sites_selected[-1].set(0)
-    tk.Checkbutton(root, text=site, variable=sites_selected[-1], onvalue=1, offvalue=0).grid(column=1, columnspan=2, row=3+i, sticky="w")
+    tk.Checkbutton(root, text=site[0], variable=sites_selected[-1], onvalue=1, offvalue=0).grid(column=1, columnspan=2, row=3+i, sticky="w")
 
 # events
 root.bind('<Return>', submit)
@@ -53,43 +67,3 @@ root.grid_rowconfigure(3+len(sites_available), weight=1, minsize=50)
 
 # run
 root.mainloop()
-
-
-
-exit()
-
-
-
-
-query = 'testiranje čć'
-
-selected_sites = [ 'ezy' ]
-
-for site, settings in config.items('sites'):
-    if site not in selected_sites:
-        continue
-
-    url, encode_type = settings.split('\n')
-
-    if encode_type == 'quote_plus':
-        encoded_query = urllib.parse.quote_plus(query)
-
-    url = url.replace('QUERY', encoded_query)
-
-    print(site)
-    print(url)
-
-exit()
-
-urls = [
-    'https://ezy.hr/katalog.aspx?action=upit&u=modem',
-    'https://www.bdeak.net/',
-    'https://git.bdeak.net/',
-]
-
-webbrowser.open_new(urls[0])
-
-time.sleep(0.5)
-
-for url in urls[1:]:
-    webbrowser.open_new_tab(url)
