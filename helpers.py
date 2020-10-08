e1fe249fd016454edcf41e395faf9212a429fb51
Bartol Deak <b@bdeak.net>
2020-10-09 00:03:02 +0200

    remove old files

--- helpers.py
+++
@@ -1,6 +0,0 @@
-def fill_input(driver, selector, value):
-	driver.execute_script('document.querySelector("%s").value = "%s"' % (selector, value))
-
-
-def click_button(driver, selector):
-	driver.execute_script('document.querySelector("%s").click()' % (selector))
