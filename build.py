5aca2922c9ed89794c49f2cfb1c3508d8c0d070c
Bartol Deak <b@bdeak.net>
2020-10-09 11:38:48 +0200

    add cxfreeze build config

---
+++ build.py
@@ -0,0 +1,23 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {
+    'packages': [], 
+    'excludes': [], 
+    'build_exe': './/search-repeater',
+    'include_files': ['icon.ico', 'config.ini']
+}
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('search-repeater.py', base=base, icon="icon.ico")
+]
+
+setup(name='search repeater',
+      version = '1.0',
+      description = '',
+      options = {'build_exe': build_options},
+      executables = executables)
