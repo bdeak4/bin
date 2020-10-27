8be922b0bc728a2a09f05fc5380042c4cd121234
Bartol Deak <b@bdeak.net>
2020-10-27 17:16:12 +0100

    add support for ignored selectors

--- webmonitor.py
+++ webmonitor.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
 
 import os, requests, csv, pathlib, hashlib
+from bs4 import BeautifulSoup
 
 rootpath = pathlib.Path(__file__).parent.absolute()
-configpath = os.path.join(rootpath, 'config')
-historypath = os.path.join(rootpath, 'history')
+configpath = os.path.join(rootpath, 'config.csv')
+historypath = os.path.join(rootpath, 'history.csv')
 
 with open(configpath, 'r') as configfile:
-    for line in configfile:
-        url = line.rstrip()
+    configreader = csv.reader(configfile)
+    for row in configreader:
+        url, selectors = row
 
         html = requests.get(url, headers = {'User-agent': 'webmonitor'}).text
+        if selectors:
+            soup = BeautifulSoup(html, 'html.parser')
+            for selector in selectors.split('|'):
+                for tag in soup.select(selector):
+                    tag.decompose()
+            html = soup.prettify()
         htmlhash = hashlib.sha256(html.encode('utf-8')).hexdigest()
 
         changed = True
         if pathlib.Path(historypath).is_file():
             with open(historypath, 'r') as historyfile:
-                reader = csv.reader(historyfile)
-                for row in reader:
+                historyreader = csv.reader(historyfile)
+                for row in historyreader:
                     historyurl, historyhash = row
                     if url == historyurl and htmlhash == historyhash:
                         changed = False
 
         if changed:
-            with open(historypath, 'a') as historyfile:
-                historyfile.write(f"{url},{htmlhash}\n")
+            # with open(historypath, 'a') as historyfile:
+            #     historyfile.write(f"{url},{htmlhash}\n")
 
             print(url)
+            # to delete
+            print(html)
