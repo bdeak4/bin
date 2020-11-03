8a7a5531c9f31403e660cc881b56ed37f50fe943
Bartol Deak <b@bdeak.net>
2020-11-03 15:45:11 +0100

    new output format

--- git-mux/git-status-recursive.sh
+++ git-mux/git-status-recursive.sh
@@ -3,9 +3,14 @@
 repositories=$(find . -type d -name .git -prune -exec dirname {} \;)
 
 for repository in $repositories; do
-	#printf "\e[36m%s\e[0m:\n" "$dir"
-	unpushed=$(git -C "$repository" log --branches --not --remotes --oneline | wc -l)
-	#printf "%s unpushed commits\n" "$unpushed"
-	git -C "$repository" status --short
-	echo
-done
+	unpushed_commits=$(git -C "$repository" log --branches --not --remotes --oneline | wc -l)
+	untracked_files=$(git -C "$repository" ls-files --others --exclude-standard | wc -l)
+	modified_files=$(git -C "$repository" diff --name-only | wc -l)
+	staged_files=$(git -C "$repository" diff --name-only --cached | wc -l)
+
+	if [ "$unpushed_commits" -gt 0 ] || [ "$untracked_files" -gt 0 ] ||
+		[ "$modified_files" -gt 0 ] || [ "$staged_files" -gt 0 ]; then
+		echo $repository:,$unpushed_commits unpushed,$untracked_files untracked,\
+			$modified_files modified,$staged_files staged
+	fi
+done | column -t -s ,
