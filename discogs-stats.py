463b8ba9151d914f147018c7d41010e80ed9532b
Bartol Deak <b@bdeak.net>
2023-01-30 02:49:21 +0100

    add year stats

--- discogs-stats.py
+++ discogs-stats.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 import requests
 import json
 import time
 
-list_id = "1192264"
+list_id = sys.argv[1]
 base_url = "https://api.discogs.com"
 
 # load cache if it exists
@@ -34,6 +35,7 @@ for record in records:
     data = {
         "genres": record["genres"] if "genres" in record else [],
         "styles": record["styles"] if "styles" in record else [],
+        "year": record["year"],
     }
     record_cache[record["resource_url"]] = data
 
@@ -43,26 +45,39 @@ for record in records:
     time.sleep(1)
 
 # process data
-genres = {}
-styles = {}
+genres, styles = {}, {}
+years, decades = {}, {}
 for record in record_cache.values():
     for genre in record["genres"]:
-        if genre in genres:
-            genres[genre] += 1
-        else:
-            genres[genre] = 1
+        genres[genre] = genres[genre] + 1 if genre in genres else 1
 
     for style in record["styles"]:
-        if style in styles:
-            styles[style] += 1
-        else:
-            styles[style] = 1
+        styles[style] = styles[style] + 1 if style in styles else 1
+
+    year = record["year"]
+    years[year] = years[year] + 1 if year in years else 1
+    decade = int(year / 10) * 10
+    decades[decade] = decades[decade] + 1 if decade in decades else 1
 
 # display results
 print("GENRES:")
 for genre, count in sorted(genres.items(), key=lambda x: x[1], reverse=True):
     print(count, genre)
 
+print()
+
 print("STYLES:")
 for style, count in sorted(styles.items(), key=lambda x: x[1], reverse=True):
     print(count, style)
+
+print()
+
+print("YEARS:")
+for year, count in sorted(years.items(), key=lambda x: x[1], reverse=True):
+    print(count, year)
+
+print()
+
+print("DECADES:")
+for decade, count in sorted(decades.items(), key=lambda x: x[1], reverse=True):
+    print(count, decade)
