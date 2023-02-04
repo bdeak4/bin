3ae0f2d28ec6afe07a7eb5a73f39ff0e21b68db4
Bartol Deak <b@bdeak.net>
2023-02-04 01:52:01 +0100

    add bump app version script

---
+++ bump-version.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    echo "Usage: $0 <major|minor|patch>"
+    exit 1
+fi
+
+old_version="v0.0.0"
+version="v0.0.0"
+
+# get the git tag in the format of v1.0.0 if it exists
+git_tag=$(git describe --tags --abbrev=0 2>/dev/null)
+
+replace_version() {
+    if [ -n "$git_tag" ]; then
+        version=$(echo $git_tag | sed 's/^v//')
+        old_version=$version
+    fi
+
+    if [ "$1" == "major" ]; then
+        # bump major version to vx+1.x.x
+        version=$(echo $version | awk -F. '{$1=$1+1; $2=0; $3=0; OFS="."; printf "v%s.%s.%s", $1,$2,$3}')
+    elif [ "$1" == "minor" ]; then
+        # bump minor version to vx.x+1.x
+        version=$(echo $version | awk -F. '{$2=$2+1; $3=0; OFS="."; printf "%s.%s.%s", $1,$2,$3}')
+    elif [ "$1" == "patch" ]; then
+        # bump patch version to vx.x.x+1
+        version=$(echo $version | awk -F. '{$3=$3+1; OFS="."; printf "%s.%s.%s", $1,$2,$3}')
+    fi
+}
+
+# if the git tag exists then bump the version by argument (major, minor, patch)
+replace_version $1
+
+RED='\033[0;31m'
+WHITE='\033[0m'
+
+echo "${RED}Bumping version from $old_version to $version"
+echo "${WHITE}"
+echo "============================="
+echo "Accept the changes? (y/n)"
+read -r answer
+
+if [ "$accept" == "y" ]; then
+    # replace version in package.json
+    sed -i '' "s/\"version\": \"$old_version\"/\"version\": \"$version\"/g" package.json
+
+    # replace in go.mod
+    sed -i '' "s/version $old_version/version $version/g" go.mod
+else
+    echo "Aborted"
+    exit 1
+fi
